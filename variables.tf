864ea95ad42bc8cf7df305a3fc84b2275ee11fcc
s3lva-kumar <98340923+s3lva-kumar@users.noreply.github.com>
2022-05-07 13:47:05 +0530

    IaC for faasd linode

---
+++ variables.tf
@@ -0,0 +1,57 @@
+variable "api_token" {
+  description = " Linode API token"
+}
+
+variable "basic_auth_password" {
+  description = "The basic auth password, if left empty, a random password is generated."
+  type        = string
+  default     = null
+  sensitive   = true
+}
+
+variable "domain" {
+  description = "Your public domain"
+  type        = string
+}
+
+variable "email" {
+  description = "Email used to order a certificate from Letsencrypt"
+  type        = string
+}
+
+variable "basic_auth_user" {
+  description = "The basic auth user name."
+  type        = string
+}
+
+variable "instance_image" {
+  description = "Image to use for Linode instance."
+  type        = string
+}
+
+variable "instance_label" {
+  description = "The region where your Linode will be located."
+  type        = string
+}
+
+variable "region" {
+  description = "The region where your Linode will be located."
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Your Linode's plan type."
+  type        = string
+}
+
+variable "root_pass" {
+  description = "Your Linode's root user's password."
+  type        = string
+  sensitive   = true
+}
+
+variable "firewall_rules" {
+  description = "List out the port number and protocol to allow in Firewall Rules"
+  type        = list(any)
+  default     = [{ protocol = "tcp", port = "22", notes = "Allow_SSH" }, { protocol = "tcp", port = "8080", notes = "Allow_HTTP" }, { protocol = "tcp", port = "443", notes = "Allow_HTTPS" }]
+}
